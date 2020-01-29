276384d77ce5a32140badb6185a3d63f02dd0b40
hamzahellowworld <hamzaaitsidisaid.11@gmail.com>
2020-01-29 10:15:33 +0100

    about us change slick js link from back to front

--- app/Http/Controllers/GaleryController.php
+++ app/Http/Controllers/GaleryController.php
@@ -8,93 +8,97 @@ use App\Gallery;
 use DB;
 class GaleryController extends Controller
 {
-    public function tester(Request $request)
-    {
-        echo 'hahwa '.$request->tab;exit;
+  public function __construct()
+  {
+    $this->middleware('auth');
+}
+public function tester(Request $request)
+{
+    echo 'hahwa '.$request->tab;exit;
 
-        echo "limage ".$request->tab;exit;
-        return 1;
-    }
+    echo "limage ".$request->tab;exit;
+    return 1;
+}
 
 /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function imageCrop()
-    {
-        return view('back.test');
-    }
+public function imageCrop()
+{
+    return view('back.test');
+}
 
-    public function imageCropPost(Request $request)
-    {
-        
-        $datas = $request->dataSrc;
-        $table_images=[];
-        $i=1;
-         foreach($datas as $data)
-         {
-            list($type, $data) = explode(';', $data);
-            list(, $data)      = explode(',', $data);
+public function imageCropPost(Request $request)
+{
     
-    
-            $data = base64_decode($data);
-          
-            $image_name= $i.'_'.time().'.png';
-           array_push( $table_images,$image_name);
-            $path = public_path('img/'). $image_name;
-            
-           file_put_contents($path, $data);
-           $i++;
-         }
+    $datas = $request->dataSrc;
+    $table_images=[];
+    $i=1;
+    foreach($datas as $data)
+    {
+        list($type, $data) = explode(';', $data);
+        list(, $data)      = explode(',', $data);
         
-      return view('back.gdetails',['images'=>$table_images]);
-       
-    }
-
-
-
-    public function create(Request $request){
-       $data=$request->data;
-       $i=1;
-      foreach($data as $dt)
-       {
-         $image=$dt["image"];
-         $description_['fr']=$dt["detail"];
-         $description_['ar']=$dt["detail"];
-         $description=json_encode($description_,JSON_UNESCAPED_UNICODE);
-         $lieu_['fr']=$dt["lieu"];
-         $lieu_['ar']=$dt["lieu_ar"];
-         $lieu=json_encode($lieu_,JSON_UNESCAPED_UNICODE);
-         DB::table('gallerys')->insert(
-            ['image' =>$image,
-             'lieu' =>$lieu,
-             'description'=>$description,
-             'position'=>$i]
-        );
+        
+        $data = base64_decode($data);
+        
+        $image_name= $i.'_'.time().'.png';
+        array_push( $table_images,$image_name);
+        $path = public_path('img/'). $image_name;
+        
+        file_put_contents($path, $data);
         $i++;
-       }
-       return redirect('gallery/list');
     }
-    public function getGallery()
+    
+    return view('back.gdetails',['images'=>$table_images]);
+    
+}
+
+
+
+public function create(Request $request){
+ $data=$request->data;
+ $i=1;
+ foreach($data as $dt)
+ {
+   $image=$dt["image"];
+   $description_['fr']=$dt["detail"];
+   $description_['ar']=$dt["detail"];
+   $description=json_encode($description_,JSON_UNESCAPED_UNICODE);
+   $lieu_['fr']=$dt["lieu"];
+   $lieu_['ar']=$dt["lieu_ar"];
+   $lieu=json_encode($lieu_,JSON_UNESCAPED_UNICODE);
+   DB::table('gallerys')->insert(
+    ['image' =>$image,
+    'lieu' =>$lieu,
+    'description'=>$description,
+    'position'=>$i]
+);
+   $i++;
+}
+return redirect('gallery/list');
+}
+public function getGallery()
+{
+    $gallerys=Gallery::all();
+    $gallerys=$gallerys->toArray();
+    foreach($gallerys as $key => $val)
     {
-        $gallerys=Gallery::all();
-        $gallerys=$gallerys->toArray();
-        foreach($gallerys as $key => $val)
-        {
-            $gallerys[$key]['lieu']=json_decode($val['lieu'],true);
-            $gallerys[$key]['description']=json_decode($val['description'],true);
+        $gallerys[$key]['lieu']=json_decode($val['lieu'],true);
+        $gallerys[$key]['description']=json_decode($val['description'],true);
          //   $gallerys[$key]['image']=$val['image'];
           //  $gallerys[$key]['id']=$val['id'];
-        }
-
-        if($gallerys){
-            return view('back.glist',['gallerys'=>$gallerys]);
-        }else{
-            $gallerys=array();
-            return view('back.glist',['gallerys'=>$gallerys]);
-        }
-        
     }
+
+    if($gallerys){
+        return view('back.glist',['gallerys'=>$gallerys]);
+    }else{
+        $gallerys=array();
+        return view('back.glist',['gallerys'=>$gallerys]);
+    }
+    
+}
 
 }
---
+++ resources/views/back/test.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ALsa</title>
+    <link href="{{asset('images/favicon.png')}}" rel="icon">
+    <link href="{{asset('css_front/new/default.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('css_front/plugins.css')}}">
+    <link rel="stylesheet" href="{{asset('css_front/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css_front/style1.css')}}">
+    <link href="{{asset('css_front/new/magnific-popup.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css_front/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css_back/amanda.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.min.css">
+
+</head>
+<body  data-spy="scroll" data-target=".navbar" data-offset="90">
+    <!-- Gallery -->
+    <section id="the-gallery" class="wide-gallery inner-gallery section-inner">
+        <div class="container">
+            <!-- section title -->
+            <div class="inner-heading">
+            </div>
+            <div class="row">
+               <div class="col-sm-4 mix">
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=380&height=209.5&position=1')}}" title="">
+                            <img src="https://dummyimage.com/380x209/000/fff" alt="">
+                            <i class="fas-upload"></i>
+                        </a>
+                    </div>
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=380&height=209.5&position=2')}}" title="">
+                            <img src="https://dummyimage.com/380x209/000/fff" alt="">
+                            <i class="fa fa-upload" aria-hidden="true" style="color:white"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4 mix">
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=380&height=209.5&position=3')}}" title="">
+                            <img src="https://dummyimage.com/380x418.2/807680/fff" alt="">
+                            <i class="fas-upload"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4 mix">
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=380&height=209&position=4')}}" title="">
+                            <img src="https://dummyimage.com/380x209/000/fff" alt="">
+                            <i class="fas-upload"></i>
+                        </a>
+                    </div>
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=380&height=209&position=5')}}" title="">
+                            <img src="https://dummyimage.com/380x209/000/fff" alt="">
+                            <i class="fas-upload"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 mix">
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=570&height=313.5&position=6')}}" title="">
+                            <img src="https://dummyimage.com/570x313.5/ebc3eb/000000" alt="">
+                            <i class="fas fa-upload"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 mix">
+                    <div class="item ">
+                        <a href="{{url('toCrop/?width=570&height=313.5&position=7')}}" title="">
+                            <img src="https://dummyimage.com/570x313.5/ebc3eb/000000" alt="">
+                            <i class="fas-upload"></i>
+                        </a>
+                    </div>
+                </div>
+         </div>
+     </div>
+ </section>
+ 
+
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{asset('js_front/jquery-3.1.1.min.js')}}"></script>
+
+<!--Bootstrap Core-->
+<script src="{{asset('js_front/popper.min.js')}}"></script>
+<script src="{{asset('js_front/bootstrap.min.js')}}"></script>
+
+<!--to view items on reach-->
+<script src="{{asset('js_front/jquery.appear.js')}}"></script>
+
+<!--Equal-Heights-->
+<script src="{{asset('js_front/jquery.matchHeight-min.js')}}"></script>
+
+<!--Owl Slider-->
+<script src="{{asset('js_front/owl.carousel.min.js')}}"></script>
+
+<!--number counters-->
+<script src="{{asset('js_front/jquery-countTo.js')}}"></script>
+
+<!--Parallax Background-->
+<script src="{{asset('js_front/parallaxie.js')}}"></script>
+
+<!--Cubefolio Gallery-->
+<script src="{{asset('js_front/jquery.cubeportfolio.min.js')}}"></script>
+
+<!--FancyBox popup-->
+<script src="{{asset('js_front/jquery.fancybox.min.js')}}"></script>          
+
+<!-- Video Background-->
+<script src="{{asset('js_front/jquery.background-video.js')}}"></script>
+
+<!--TypeWriter-->
+<script src="{{asset('js_front/typewriter.js')}}"></script> 
+
+<!--Particles-->
+<script src="{{asset('js_front/particles.min.js')}}"></script>            
+
+<!--WOw animations-->
+<script src="{{asset('js_front/wow.min.js')}}"></script>
+
+<!--Revolution SLider-->
+<script src="{{asset('js_front/revolution/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/jquery.themepunch.revolution.min.js')}}"></script>
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.actions.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.carousel.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.kenburn.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.layeranimation.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.migration.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.navigation.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.parallax.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.slideanims.min.js')}}"></script>
+<script src="{{asset('js_front/revolution/extensions/revolution.extension.video.min.js')}}"></script>
+<script src="{{asset('lib/popper.js/popper.js')}}"></script>
+<script src="{{asset('lib/bootstrap/bootstrap.js')}}"></script>
+<script src="{{asset('lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js')}}"></script>
+<script src="{{asset('lib/jquery-toggles/toggles.min.js')}}"></script>
+<script src="{{asset('lib/highlightjs/highlight.pack.js')}}"></script>
+
+<script src="{{asset('/js/amanda.js')}}"></script>      
+<!--Google Map API-->
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAfzJrG8u_tUgBqK5M61cj_-ytj68rJwZA&language=ar"></script>
+<script src="{{asset('js_front/functions.js')}}"></script>
+<!-- Magnific Popup JS --><script src="{{asset('js_front/new/jquery.magnific-popup.min.js')}}"></script>
+<!-- custom image popup Jquery -->
+<script src="{{asset('js_front/new/custom-magnificpopup.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.js"></script>
+<script type="text/javascript">
+
+var resize = $('#upload-demo').croppie({
+    enableExif: true,
+    enableOrientation: true,    
+    viewport: { // Default { width: 100, height: 100, type: 'square' } 
+        width: 200,
+        height: 200,
+       // type: 'circle' //square
+    },
+    boundary: {
+        width: 300,
+        height: 300
+    }
+});
+
+
+$('#image').on('change', function () { 
+  var reader = new FileReader();
+    reader.onload = function (e) {
+      resize.croppie('bind',{
+        url: e.target.result
+      }).then(function(){
+        console.log('jQuery bind complete');
+      });
+    }
+    reader.readAsDataURL(this.files[0]);
+});
+
+
+$('.btn-info').on('click', function (ev) {
+  resize.croppie('result', {
+    type: 'canvas',
+    size: 'viewport'
+  }).then(function (img) {
+     var image_cr=$('#image_part').val(img);
+      alert(image_cr);
+      $('#formPart').submit();
+  });
+});
+</script>
+</body>
+</html>
